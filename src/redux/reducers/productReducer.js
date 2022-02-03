9de06c8127ffade8f7edf929bc416b40af6913af
itshmnt <hemantxs5@gmail.com>
2022-02-03 12:15:31 +0530

    Cennection with redux

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -10,7 +10,7 @@ const initialstate = {
     ]
 };
 
-export const productReducer = (state, {type, payload}) => {
+export const productReducer = (state = initialstate, {type, payload}) => {
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
             return state;
